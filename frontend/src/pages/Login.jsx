68dacff5367d445faa9d9631af04f779b07f8139
jaashannn <jaashannn@gmail.com>
2024-12-12 22:41:08 +0530

    something

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://working-tg-crm.onrender.com/api/auth/login",
         { email, password }
       );
       if (response.data.success) {
